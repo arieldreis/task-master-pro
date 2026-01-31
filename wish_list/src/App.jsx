96c570061ef297b374d8ae1cbf4c0eac9bd6264f
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-01-30 22:44:00 -0300

    feat: Enhance styling and layout for the wishlist application

--- wish_list/src/App.jsx
+++ wish_list/src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
+import e from 'express';
 
 function App(){
+  const [task, setTask] = useState("");
+  function addTask(){}
+  
   return (
     <>
       <div className="titlesContainer">
         <h1>My WishList</h1>
-        <button onClick={() => window.alert("Oi")}>Remove</button>
       </div>
       <div className="containerAddWish">
         <label htmlFor="idWishList">Type:</label>
-        <input type="text" placeholder='Add Item' id='idWishList'/>
-        <button>ADD</button>
+        <input type="text" placeholder='Add Item' value={task} onChange={e => setTask(e.target.value)}/>
+        <button onClick={addTask}>ADD</button>
+        <div className='ElementsWishList'>
+          <div className='box'>ccccc</div>
+          <div className='box'>ccccc</div>
+          {/* Criar uma tag div dinamicamente e colocar com uma classe box */}
+        </div>
       </div>
     </>
   )
