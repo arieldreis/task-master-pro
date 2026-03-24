7f37d1fdbc73723dac2521c1737f7d8a3b3c1bf8
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-03-24 10:18:26 -0300

    fix: Update favicon path in index.html and add table styles in App.css; refactor Tabela component for data fetching and rendering

--- wish_list/src/Tabela.jsx
+++ wish_list/src/Tabela.jsx
@@ -1,29 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Tabela = () => {
+  const [dados, setDados] = useState([]);
   const url = "http://localhost:8080/listar";
-  const Req_API = (url) => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        const arrayData = Object.values(data);
-        arrayData.forEach((user) => {
-          console.log(`Modelo: ${user.marca}, Marca: ${user.nome}`);
+
+  useEffect(() => {
+    const Req_API = (url) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          const arrayData = Array.isArray(data) ? data : Object.values(data);
+          setDados(arrayData);
+        })
+        .catch((error) => {
+          console.log("Erro interno no servidor." + error);
         });
-      })
-      .catch((error) => {
-        console.log("Erro interno no servidor." + error);
-      });
-  };
-  // Put the endpoint inside the function.
-  Req_API(url);
+    };
+    
+    // Put the endpoint inside the function.
+    Req_API(url);
+  });
+
   return (
     <>
-        <div>
-            
-        </div>
+      <div class="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>Código</th>
+              <th>Marca</th>
+              <th>Nome</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                dados.map((products) => (
+                    <tr key={dados.id}>
+                        <td>{products.codigo}</td>
+                        <td>{products.marca}</td>
+                        <td>{products.nome}</td>
+                    </tr>
+                ))
+            }
+          </tbody>
+        </table>
+      </div>
     </>
-  )
+  );
 };
 
 export default Tabela;
